e72e4715256ca7c0972f62a714eb2def5ddf389f
Tangb <tanguy.bonneau@gmail.com>
2019-05-21 00:30:32 +0200

    Remove tests script. Now tests execution is handled by cleep-cli

--- tests/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Running tests:" && coverage run --omit="/usr/local/lib/python2.7/*","../backend/*Event.py" --source="../backend" --concurrency=thread test_*.py && echo ""; echo "Code coverage:" && coverage report -m 
-
